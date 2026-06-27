c083319a85c238bb114a5229712aed9470caa2b7
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 00:21:03 +0630

    Create FAQPage.jsx

---
+++ src/pages/FAQPage.jsx
@@ -0,0 +1,11 @@
+import FAQ from '../components/FAQ/FAQ.jsx';
+import Contact from '../components/Contact/Contact.jsx';
+
+export default function FAQPage() {
+  return (
+    <>
+      <FAQ />
+      <Contact />
+    </>
+  );
+}
