ae7d5d2abe6e3b6a9ea0ee431c7bc246f3822419
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 00:19:49 +0630

    Create PricingPage.jsx

---
+++ src/pages/PricingPage.jsx
@@ -0,0 +1,11 @@
+import Pricing from '../components/Pricing/Pricing.jsx';
+import FAQ from '../components/FAQ/FAQ.jsx';
+
+export default function PricingPage() {
+  return (
+    <>
+      <Pricing />
+      <FAQ />
+    </>
+  );
+}
