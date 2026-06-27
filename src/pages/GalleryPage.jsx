839a0ac2fab71604ad21d58653cf52edcf2cbbe6
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 00:21:25 +0630

    Create GalleryPage.jsx

---
+++ src/pages/GalleryPage.jsx
@@ -0,0 +1,11 @@
+import Gallery from '../components/Gallery/Gallery.jsx';
+import Projects from '../components/Projects/Projects.jsx';
+
+export default function GalleryPage() {
+  return (
+    <>
+      <Gallery />
+      <Projects />
+    </>
+  );
+}
