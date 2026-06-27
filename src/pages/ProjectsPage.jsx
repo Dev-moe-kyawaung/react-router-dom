f3e0edab82a17b56833e6459de74be946991299f
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 00:19:02 +0630

    Create ProjectsPage.jsx

---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,11 @@
+import Projects from '../components/Projects/Projects.jsx';
+import Gallery from '../components/Gallery/Gallery.jsx';
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <Projects />
+      <Gallery />
+    </>
+  );
+}
