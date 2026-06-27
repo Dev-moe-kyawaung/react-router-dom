2d30dc54f212861f95f9cde8382981765e372560
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 00:00:06 +0630

    Create useTheme.js

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+export function useTheme() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+  useEffect(() => {
+    document.documentElement.dataset.theme = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+  return { theme, setTheme };
+}
